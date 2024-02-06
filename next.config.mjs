3972eb5408b194aa2b137ae658d16f19779faf71
Angel Lopez <angelcecyt09@gmail.com>
2024-02-05 18:35:48 -0600

    test 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.export = {
-    output: "export",
-    // ...
-}
+const nextConfig = {
+    output:"export"
+};
 module.exports = nextConfig
-export default nextConfig;
