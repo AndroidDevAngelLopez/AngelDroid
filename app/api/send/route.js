555f5c9641b3686bc92482771f39742595336083
Angel Lopez <angelcecyt09@gmail.com>
2024-02-05 18:48:40 -0600

    test 3

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+const resend = new Resend("re_egxxmpVF_4VhNVCJrh6NGqneg8SnZZzkz");
+const fromEmail = "angelcecyt09@gmail.com";
 
 export async function POST(req, res) {
     const { email, subject, message } = await req.json();
