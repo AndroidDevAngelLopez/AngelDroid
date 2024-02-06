cc7f436c967bdddd5cd774dc6e9ff09d3980a0cf
Angel Lopez <angelcecyt09@gmail.com>
2024-02-05 18:46:54 -0600

    test 3

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,10 +1,6 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
-
-const re_egxxmpVF_4VhNVCJrh6NGqneg8SnZZzkz = () => {
-
-};
-const resend = new Resend(re_egxxmpVF_4VhNVCJrh6NGqneg8SnZZzkz);
+const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
