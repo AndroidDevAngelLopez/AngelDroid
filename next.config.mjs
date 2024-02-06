279afd71ca054106f1d156622553bce6703d30df
Angel Lopez <angelcecyt09@gmail.com>
2024-02-06 17:35:31 -0600

    test 7

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: "export",
+//     reactStrictMode: true,
+//     images: {
+//         unoptimized: true
+//     }
+// };
+//
+// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    reactStrictMode: true,
+    output:'export',
     images: {
         unoptimized: true
     }
 };
-
-module.exports = nextConfig;
+/* module.exports = nextConfig*/
+export default nextConfig;
